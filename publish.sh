9980a7897ab77f07bee7e0e48026f6d8ec9f4618
Michael Ottoson <michael@pointw.com>
2025-03-07 12:05:26 -0500

    publish now publishes automatically

--- publish.sh
+++ publish.sh
@@ -6,11 +6,5 @@ rm -rf ~/aws/dist
 mkdir ~/aws/dist
 cp -ruT .vitepress/dist/ ~/aws/dist
 
-echo
-echo ----------
-echo jumping into pointw-aws so you can complete the publish
-echo  - change into /work then run publish-pointw
-echo ----------
-
 docker start pointw-aws
-docker exec -it pointw-aws bash
+docker exec -it -w /work pointw-aws ./publish-pointw
